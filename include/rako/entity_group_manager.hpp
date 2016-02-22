ff188c14c1c1136957121fd3a1398168a3f8e700
metagoto <runpac314@gmail.com>
2016-02-22 23:54:55 +0100

    egm: constexpr dispatch

--- include/rako/entity_group_manager.hpp
+++ include/rako/entity_group_manager.hpp
@@ -37,84 +37,77 @@ namespace rako
     template <typename Traits, std::size_t... Is, typename... Ts>
     struct group_id_expand<Traits, std::index_sequence<Is...>, Ts...>
     {
-      using type = std::tuple<component_group<Is, Ts, component_group_traits>...>;
+      using type = std::tuple<component_group<Is, Ts, Traits>...>;
     };
   }
 
 
-
-  template <typename... CGLs> // meta::list<T1,T2>, meta::list<T1,T3>, ...
+  template <typename... CGLs>
   struct entity_group_manager
   {
+    using self_t = entity_group_manager<CGLs...>;
+
     using group_tuple = typename impl::group_id_expand<component_group_traits,
       std::index_sequence_for<CGLs...>, CGLs...>::type; // TODO: traits!
 
+    using group_index_seq = std::make_index_sequence<std::tuple_size<group_tuple>::value>;
     using group_list = meta::as_list<group_tuple>;
 
     using handle = typename std::tuple_element<0, group_tuple>::type::handle;
 
-    group_tuple groups;
 
+    group_tuple groups;
     std::vector<handle> killed;
 
-
+    ///
+    template <typename... Cs>
+    struct group_for_comp
+    {
+      using comp = meta::list<std::decay_t<Cs>...>;
+      using type = meta::front<meta::find_if<group_list,
+        meta::lambda<meta::placeholders::_a, impl::is_group<meta::placeholders::_a, comp>>>>;
+    };
     template <typename... Ts>
     auto add(Ts&&... ts)
     {
       return add(std::forward_as_tuple(std::forward<Ts>(ts)...));
     }
-
     template <typename... Ts>
     auto add(std::tuple<Ts...>&& t)
     {
-      using CL = meta::list<std::decay_t<Ts>...>;
-      using meta::placeholders::_a;
-      using cgl = meta::find_if<group_list, meta::lambda<_a, impl::is_group<_a, CL>>>;
-      using cg = meta::front<cgl>;
-      return std::get<cg>(groups).add(std::move(t));
+      return std::get<typename group_for_comp<Ts...>::type>(groups).add(std::move(t));
     }
-
     template <typename... Ts>
     auto add(std::tuple<Ts...> const& t)
     {
-      using CL = meta::list<std::decay_t<Ts>...>;
-      using meta::placeholders::_a;
-      using cgl = meta::find_if<group_list, meta::lambda<_a, impl::is_group<_a, CL>>>;
-      using cg = meta::front<cgl>;
-      return std::get<cg>(groups).add(t);
+      return std::get<typename group_for_comp<Ts...>::type>(groups).add(t);
     }
-
     template <typename... Ts>
     auto add(std::tuple<Ts...>& t)
     {
-      using CL = meta::list<std::decay_t<Ts>...>;
-      using meta::placeholders::_a;
-      using cgl = meta::find_if<group_list, meta::lambda<_a, impl::is_group<_a, CL>>>;
-      using cg = meta::front<cgl>;
-      return std::get<cg>(groups).add(t);
+      return std::get<typename group_for_comp<Ts...>::type>(groups).add(t);
     }
 
     ///
-    template <std::size_t... Is>
-    void remove_impl(handle& h, std::index_sequence<Is...>)
+    template <std::size_t I>
+    void remove_impl(group_tuple& t, handle& h)
     {
-      using p = void(group_tuple&, handle&);
-      static p* a[] = {[](group_tuple& g, handle& h)
-        {
-          std::get<Is>(g).erase(h);
-        }...};
-      a[h.group()](groups, h);
+      std::get<I>(t).erase(h);
     }
-
+    template <std::size_t... Is>
+    void remove_dispatch(handle& h, std::index_sequence<Is...>)
+    {
+      static constexpr decltype(&self_t::remove_impl<0>) a[] = {&self_t::remove_impl<Is>...};
+      (this->*(a[h.group()]))(groups, h);
+    }
     void remove(handle& h)
     {
       if (!valid(h)) return;
       constexpr auto s = std::tuple_size<group_tuple>::value;
-      remove_impl(h, std::make_index_sequence<s>{});
+      remove_dispatch(h, std::make_index_sequence<s>{});
     }
 
-
-    //
+    ///
     void kill(handle const& h) // TODO: multiple kill
     {
       killed.push_back(h);
@@ -130,35 +123,27 @@ namespace rako
       killed.clear();
     }
 
-    //
+    ///
+    template <std::size_t I>
+    auto valid_impl(group_tuple const& t, handle const& h) const
+    {
+      return std::get<I>(t).valid(h);
+    }
     template <std::size_t... Is>
-    bool valid_impl(handle const& h, std::index_sequence<Is...>) const
+    auto valid_dispatch(handle const& h, std::index_sequence<Is...>) const
     {
-      using p = bool(group_tuple const&, handle const&);
-      static p* a[] = {[](group_tuple const& g, handle const& h) -> bool
-        {
-          return std::get<Is>(g).valid(h);
-        }...};
-      return a[h.group()](groups, h);
+      static constexpr decltype(&self_t::valid_impl<0>) a[] = {&self_t::valid_impl<Is>...};
+      return (this->*(a[h.group()]))(groups, h);
     }
+    auto valid(handle const& h) const { return valid_dispatch(h, group_index_seq{}); }
 
-    bool valid(handle const& h) const
-    {
-      constexpr auto s = std::tuple_size<group_tuple>::value;
-      return valid_impl(h, std::make_index_sequence<s>{});
-    }
-
+    ///
     bool alive(handle const& h) const
     {
       return valid(h) && std::find(std::begin(killed), std::end(killed), h) == std::end(killed);
     }
 
-
-    //  template <typename T = void>
-    //  std::enable_if_t<std::is_same<T, void>::value, std::size_t> size() const
-    //  {
-    //    return entity.size();
-    //  }
+    ///
     template <typename...>
     struct size_impl;
     template <typename... Ts>
@@ -172,55 +157,52 @@ namespace rako
     };
     auto size() const { return size_impl<group_tuple>::call(*this); }
 
-
     ///
-    template <typename T, std::size_t... Is>
-    auto& get_impl(handle const& h, std::index_sequence<Is...>)
+    template <typename T, std::size_t I>
+    auto& get_impl(group_tuple& t, handle const& h)
     {
-      using p = T*(group_tuple&, handle const&);
-      static p* a[] = {[](group_tuple& g, handle const& h) -> T*
-        {
-          return std::get<Is>(g).template get<T>(h);
-        }...};
-      return *a[h.group()](groups, h);
+      return *std::get<I>(t).template get<T>(h);
     }
-
+    template <typename T, std::size_t... Is>
+    auto& get_dispatch(handle const& h, std::index_sequence<Is...>)
+    {
+      static constexpr decltype(&self_t::get_impl<T, 0>) a[] = {&self_t::get_impl<T, Is>...};
+      return (this->*(a[h.group()]))(groups, h);
+    }
     template <typename T>
     auto& get(handle const& h)
     {
-      constexpr auto s = std::tuple_size<group_tuple>::value;
-      return get_impl<T>(h, std::make_index_sequence<s>{});
+      return get_dispatch<T>(h, group_index_seq{});
     }
 
-    template <typename T, std::size_t... Is>
-    auto const& get_impl(handle const& h, std::index_sequence<Is...>) const
+    ///
+    template <typename T, std::size_t I>
+    auto const& get_c_impl(group_tuple const& t, handle const& h) const
     {
-      using p = T const*(group_tuple const&, handle const&);
-      static p* a[] = {[](group_tuple const& g, handle const& h) -> T const*
-        {
-          return std::get<Is>(g).template get<T>(h);
-        }...};
-      return *a[h.group()](groups, h);
+      return *std::get<I>(t).template get<T>(h);
     }
-
+    template <typename T, std::size_t... Is>
+    auto const& get_dispatch(handle const& h, std::index_sequence<Is...>) const
+    {
+      static constexpr decltype(&self_t::get_c_impl<T, 0>) a[] = {&self_t::get_c_impl<T, Is>...};
+      return (this->*(a[h.group()]))(groups, h);
+    }
     template <typename T>
     auto const& get(handle const& h) const
     {
-      constexpr auto s = std::tuple_size<group_tuple>::value;
-      return get_impl<T>(h, std::make_index_sequence<s>{});
+      return get_dispatch<T>(h, group_index_seq{});
     }
-
 
     ///
     template <bool, typename...>
     struct for_each_impl;
-    template <bool H, typename... Cs, typename T>
-    struct for_each_impl<H, meta::list<Cs...>, T>
+    template <bool Handle, typename... Cs, typename T>
+    struct for_each_impl<Handle, meta::list<Cs...>, T>
     {
       template <typename Self, typename F>
       static void call(Self& self, F&& f)
       {
-        (std::get<Cs>(self.groups).template for_each<H, T>(std::forward<F>(f)), ...);
+        (std::get<Cs>(self.groups).template for_each<Handle, T>(std::forward<F>(f)), ...);
       }
     };
 
@@ -228,10 +210,10 @@ namespace rako
     void for_each(F&& f)
     {
       using meta::placeholders::_a;
-      using H = meta::in<L, handle>;
-      using LL = meta::filter<L, meta::lambda<_a, meta::lazy::not_<std::is_same<_a, handle>>>>;
-      using C = meta::filter<group_list, meta::lambda<_a, impl::is_comp<_a, LL>>>;
-      for_each_impl<H{}, C, LL>::call(*this, std::forward<F>(f));
+      using hand = meta::in<L, handle>;
+      using list = meta::filter<L, meta::lambda<_a, meta::lazy::not_<std::is_same<_a, handle>>>>;
+      using select = meta::filter<group_list, meta::lambda<_a, impl::is_comp<_a, list>>>;
+      for_each_impl<hand{}, select, list>::call(*this, std::forward<F>(f));
     }
   };
 }
